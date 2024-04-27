e8f5e501e80dfc34811c7d2e88bc05cdc60eb2d9
SrBaliardo <tao.john007@icloud.com>
2024-04-27 20:07:36 -0300

    Update code and images

--- js/btnTop.js
+++ js/btnTop.js
@@ -2,10 +2,7 @@ const btnToTop = document.querySelector('.btn-top')
 
 function toTop(event) {
     event.preventDefault()
-    window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-    })
+    window.scrollTo({top: 0, behavior: 'smooth'})
 }
 
 btnToTop.addEventListener('click', toTop)
